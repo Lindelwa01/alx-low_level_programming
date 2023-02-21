83e50744f9d42b57a6dcc418e86b55aae9f266fd
Lindelwa01 <anelegasa@icloud.com>
2023-02-21 14:48:06 -0800

    0x02. C

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * Return: 0
+ * main - Prints _putchar followed by a new line
+ *_putchar - writes character c to stdout
+ *@c: character to print 
+ *Return: 0 
+ */
+int _putchar(char a)
+{
+return (write(1, &c, 1));
+}
